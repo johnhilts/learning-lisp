7496dd1513dfdae315031d04ac73b0000037377d
John Hilts <johnhilts@live.com>
2020-03-09 10:27:45 -0700

    more recursion

--- gentle-intro/recursive.lisp
+++ gentle-intro/recursive.lisp
@@ -10,3 +10,71 @@
 	  (oddp (car x))
 	  (my-alloddp (cdr x))))
 	(t nil)))
+
+(defun recursive-member (find list)
+  "NIH member with recursion"
+  (if (null list) nil
+      (if (equal find (car list))
+	  list
+	  (recursive-member find (cdr list)))))
+
+(defun recursive-assoc (find table)
+  "NIH assoc with recursion"
+  (if (null table) nil
+      (if (equal find (car (car table)))
+	  (car table)
+	  (recursive-assoc find (cdr table)))))
+
+(defun recursive-nth (index list)
+  "NIH nth with recursion"
+  (if (equal 0 index) (car list)
+      (recursive-nth (- index 1) (cdr list))))
+
+; Fib(n) = Fib(n-1) + Fib(n-2)
+
+(defun my-fibonacci (n)
+  "recursive fibonacci"
+  (cond
+    ((or
+     (equal 0 n)
+     (equal 1 n)) 1)
+    (t (+ (my-fibonacci (- n 1)) (my-fibonacci (- n 2))))))
+
+(defun anyoddp (x)
+  (cond ((null x) nil)
+	((oddp (first x)) t)
+	(t (anyoddp (rest x)))))
+
+(defun find-first-odd-num (list)
+  "find the 1st odd number in the list"
+  (cond ((null list) nil)
+	((oddp (car list)) (car list))
+	(t (find-first-odd-num (cdr list)))))
+
+(defun find-last-element-in-list (list)
+  "find the last element in a list"
+  (cond ((null (cdr list)) (car list))
+	(t (find-last-element-in-list (cdr list)))))
+
+(defun add-nums (number)
+  "add the numbers"
+  (cond ((equal 0 number) 0)
+	(t (+ number (add-nums (- number 1))))))
+
+(defun all-equal? (list)
+  "are all elements in a list equal?"
+  (cond ((<= (length list) 1) t)
+	((and
+	  (equal (car list) (cadr list))
+	  (all-equal? (cdr list))))
+	(t nil)))
+
+(defun do-a-count-down (number)
+  "count down from a number using cons recursion"
+  (cond ((equal 0 number) '(0))
+	(t (cons number (do-a-count-down (- number 1))))))
+
+(defun do-another-count-down (number)
+  "count down from a number with a different kind of recursion"
+  (cond ((equal 0 number) '(0))
+	(t (append (list number) (do-another-count-down (- number 1))))))
