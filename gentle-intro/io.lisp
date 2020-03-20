af5aa1834a71f9ea1c6153abd64d40bac63a549f
John Hilts <johnhilts@live.com>
2020-03-20 15:31:44 -0700

    more input-output

--- gentle-intro/io.lisp
+++ gentle-intro/io.lisp
@@ -2,3 +2,10 @@
   "draw a line n times; use recursion"
   (cond ((zerop n) nil)
 	(t (format t "*")(draw-line (- n 1)))))
+
+(defun draw-box (length height)
+  "draw a box with the given dimensions and draw-line"
+  (cond ((zerop height) nil)
+	(t (draw-line length)
+	   (format t "~&")
+	   (draw-box length (- height 1)))))
