a029153063afbd57b3e3b08c7575dcabdaa4c923
John Hilts <johnhilts@live.com>
2020-04-02 10:02:54 -0700

    defstruct make-* getter

--- gentle-intro/structures-and-types.lisp
+++ gentle-intro/structures-and-types.lisp
@@ -8,3 +8,12 @@
 
 (defun init ()
   (setf *node-list* nil))
+
+(defun add-node (name question yes-case no-case)
+  (setf node (make-node :name name
+			:question question
+			:yes-case yes-case
+			:no-case no-case))
+  (push node *node-list*)
+  (node-name node))
+; it's also possible to push directly onto the list
