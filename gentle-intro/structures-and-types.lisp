8a5d3b6f74f257065a2c35ccbd0458dd762324e4
John Hilts <johnhilts@live.com>
2020-04-03 09:13:29 -0700

    getter dolist when return nested lets

--- gentle-intro/structures-and-types.lisp
+++ gentle-intro/structures-and-types.lisp
@@ -9,6 +9,32 @@
 (defun init ()
   (setf *node-list* nil))
 
+(defun populate-nodes ()
+  (add-node 'start
+	    "Does the engine turn over?"
+	    'engine-turns-over
+	    'engine-wont-turn-over)
+  (add-node 'engine-turns-over
+	    "Will the engine run for any period of time?"
+	    'engine-will-run-briefly
+	    'engine-wont-run)
+  (add-node 'engine-wont-run
+	    "Is there gas in the tank?"
+	    'gas-in-tank
+	    "Fill the tank and try starting the engine again.")
+  (add-node 'engine-wont-turn-over
+	    "Do you hear any sound when you turn the key?"
+	    'sound-when-turn-key
+	    'no-sound-when-turn-key)
+  (add-node 'no-sound-when-turn-key
+	    "Is the battery voltage low?"
+	    "Replace the battery"
+	    'battery-voltage-ok)
+  (add-node 'battery-voltage-ok
+	    "Are the battery cables dirty or loose?"
+	    "Clean the cables and tighten the connections."
+	    'battery-cables-good) )
+
 (defun add-node (name question yes-case no-case)
   (setf node (make-node :name name
 			:question question
@@ -16,4 +42,20 @@
 			:no-case no-case))
   (push node *node-list*)
   (node-name node))
-; it's also possible to push directly onto the list
+; it's also possible to push directly onto the list without the separate variable
+
+(defun find-node (name list)
+  (dolist (node list)
+    (when (equal (node-name node) name)
+      (return node))))
+
+(defun process-node (name)
+  (let ((node (find-node name *node-list*)))
+    (if (null node)
+	(format t "~s was not found" name)
+	(progn
+	  (format t "~s " (node-question node))
+	  (let ((reply (read)))
+	    (if (equal reply 'y)
+		(process-node (node-yes-case node))
+		(process-node (node-no-case node))))))))
