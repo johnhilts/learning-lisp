afc7bda84d2783fdbed8965db65a18e6d18187f4
John Hilts <johnhilts@live.com>
2020-04-04 17:11:53 -0700

    arrays, hashmaps & property lists - dotimes format ~2s ~3s make-array aref

--- gentle-intro/vectors.lisp
+++ gentle-intro/vectors.lisp
@@ -4,7 +4,9 @@
 
 (defun new-histogram (number-of-bins)
   (setf *total-points* 0)
-  (setf *hist-array* (make-array number-of-bins)))
+  (setf *hist-array* (make-array number-of-bins))
+  (dotimes (i 200)
+    (record-value (random number-of-bins)))))
 
 (defun record-value (n)
   (if (and (>= n 0)
@@ -13,3 +15,14 @@
 	(incf (aref *hist-array* n))
 	(incf *total-points*))
       (format t "error - input out of range")))
+
+(defun print-hist-line (n)
+  (let ((count (aref *hist-array* n)))
+    (format t "~&~2s [ ~3s] " n count)
+    (dotimes (i count)
+      (format t "*"))))
+
+(defun print-histogram ()
+  (dotimes (i (length *hist-array*))
+    (print-hist-line i)))
+  
