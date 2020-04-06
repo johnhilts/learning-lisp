f22b0a69c72451280bb018756d270c53a9d2f9d8
John Hilts <johnhilts@live.com>
2020-04-06 00:01:10 -0700

    arrays, hashmaps & property lists - gethash clrhash let* aref

--- gentle-intro/vectors-2.lisp
+++ gentle-intro/vectors-2.lisp
@@ -10,5 +10,32 @@
   (setf *encipher-table* (make-hash-table))
   (setf *decipher-table* (make-hash-table)))
 
-(defun make-substitution ()
-  )
+(defun make-substitution (code clear)
+  (setf (gethash code *decipher-table*) clear)
+  (setf (gethash clear *encipher-table*) code))
+
+(defun undo-substitution (code)
+  (let ((clear (gethash code *decipher-table*)))
+    (setf (gethash code *decipher-table*) nil)
+    (setf (gethash clear *encipher-table*) nil)))
+
+(defun clear ()
+  (clrhash *encipher-table*)
+  (clrhash *decipher-table*))
+
+(defun decipher-string (encoded-string)
+  (let* ((len (length crypto-text))
+	 (new-string (make-string len :initial-element #\Space)))
+    (dotimes (i len)
+      (let* ((code (gethash encoded-string *encipher-table*))
+	     (clear (gethash code *decipher-table*)))
+	(if clear
+	    (setf (aref new-string i) clear))))))
+
+(defun show-line ()
+  (format t "~s~%" crypto-text)
+  (let ((len (length crypto-text)))
+    (dotimes (i len)
+      (format t "~s" (gethash
+		      (gethash
+		       (aref crypto-text i) *encipher-table*) *decipher-table*)))))
