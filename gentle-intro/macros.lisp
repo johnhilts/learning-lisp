275f1ea6bf2eb7fe4b8cfbd9d0a6b4f258f4bb36
John Hilts <johnhilts@live.com>
2020-04-07 22:52:29 -0700

    macros defmacro

--- gentle-intro/macros.lisp
+++ gentle-intro/macros.lisp
@@ -23,3 +23,9 @@
 (defmacro simpler-incf (var &optional (amount 1))
   "using back-tick and , for unquote"
   `(setq ,var (+ ,var ,amount)))
+
+(defmacro simple-rotatef (a b)
+  `(let ((c ,a)
+	 (d ,b))
+     (setq ,a d)
+     (setq ,b c)))
