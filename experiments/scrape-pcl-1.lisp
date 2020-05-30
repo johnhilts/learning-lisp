bb011255860569616d777883859075d88d6bf880
John Hilts <johnhilts@live.com>
2020-05-29 20:02:15 -0700

    PCL scraper - (let*)

--- experiments/scrape-pcl-1.lisp
+++ experiments/scrape-pcl-1.lisp
@@ -5,18 +5,19 @@
 (defparameter *pcl1* (drakma:http-request *base-url*))
 (defparameter *link-file* "/home/jfh/code/lisp/source/learning-lisp/experiments/my-links.txt")
 
-(defun get-links (&optional start)
+(defun get-links (start pattern)
   (multiple-value-bind
         (start end)
-      (cl-ppcre:scan "[\\w_-]+\\.html" *pcl1* :start start)
+      (cl-ppcre:scan pattern *pcl1* :start start)
     (unless (null start)
       (let ((format-link
              #'(lambda ()
                  (let ((page (subseq *pcl1* start end)))
                    (format nil "~a~a" *base-url* page)))))
-        (append (list (funcall format-link)) (get-links end))))))
+        (append (list (funcall format-link)) (get-links end pattern))))))
 
 (defun scrape ()
-  (let ((links (get-links 0)))
+  (let* ((pattern (ppcre:create-scanner "[\\w_-]+\\.html"))
+         (links (get-links 0 pattern)))
     (with-open-file (stream *link-file* :direction :output :if-exists :supersede)
-      (format stream "~{~&~a~}" links)))))
+      (format stream "~{~&~a~}" links))))
