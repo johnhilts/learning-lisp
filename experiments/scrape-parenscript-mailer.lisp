cf5a39fc2c5855037839ddc89cff0962f4701a31
John Hilts <johnhilts@live.com>
2020-11-12 22:25:27 -0800

    parenscript mailing list scraper tweaks

--- experiments/scrape-parenscript-mailer.lisp
+++ experiments/scrape-parenscript-mailer.lisp
@@ -24,16 +24,30 @@
         (start end)
       (cl-ppcre:scan pattern *pcl1* :start start)
     (unless (null start)
-      (let ((format-link #'(lambda () (subseq *pcl1* start end))))
+      (let ((format-link #'(lambda () (subseq *pcl1* (+ 1 start) end))))
         (append (list (funcall format-link)) (get-links end pattern))))))
 
 (defun build-url (link)
   "combine base URL with relative path link"
   (format nil "~a~a" *base-url* link))
 
+(defun string-replace (string search replace)
+  (labels
+      ((replace-r (string search replace)
+         (if (zerop (length string))
+             string
+             (let ((search-position (search search string)))
+               (if (null search-position)
+                   string
+                   (concatenate 'string
+                                (subseq string 0 search-position)
+                                replace
+                                (replace-r (subseq string  (+ search-position (length search))) search replace)))))))
+    (replace-r string search replace)))
+
 (defun fetch-html (link)
   "fetch html for given relative path (will combine with base URL)"
-  (let ((html-file (format nil "~a/~a" (get-app-dir) link))
+  (let ((html-file (format nil "~a/~a" (get-app-dir) (string-replace link "/" "-")))
         (html (drakma:http-request (build-url link))))
     (with-open-file
         (stream html-file :direction :output :if-exists :supersede)
@@ -45,7 +59,7 @@
   (format t "~&Parenscript Mailing List Archive Scraper v0.1~%Using directory: ~a" (get-app-dir))
   (format t "~&my-parenscript-mailinglist-links.txt full path: ~a" (get-link-filename))
   
-  (let* ((pattern (ppcre:create-scanner "[\\w_-]+\\.html"))
+  (let* ((pattern (ppcre:create-scanner "\"[\\w/-]+\\.html"))
           (links (get-links 0 pattern)))
     (mapcar #'fetch-html links)
     (with-open-file
