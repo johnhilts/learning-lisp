f7dd36710a9718f6c6004a95e545b2fc6a9180cd
John Hilts <johnhilts@live.com>
2022-01-01 08:07:52 -0800

    sicp - Pascal's triangle

---
+++ sicp/ch1.lisp
@@ -0,0 +1,42 @@
+(defpackage #:sicp-repl
+  (:use #:cl))
+
+;; (in-package #:sicp-repl)
+
+(defun generate-pascals-triangle (&optional (levels-deep 5))
+  "Generate Pascal's Triangle n levels deep Pascal's Triangle has all
+1s on the edges, and each node is calculated by its 2 parents.  Use 0
+if a parent is missing."
+  (labels ((generate-line (previous-line)
+           (let ((previous-node 0))
+             (append
+              (mapcar #'(lambda (e)
+                          (let ((sum (+ previous-node e)))
+                            (setf previous-node e)
+                            sum))
+                      previous-line)
+              (list 1))))
+         (generate-triangle ()
+           (do
+            ((i 0 (incf i))
+             (triangle (list (list 1))))
+            ((> i levels-deep) (reverse triangle))
+             (push (generate-line (car triangle)) triangle))))
+    (let ((triangle (generate-triangle)))
+      (mapc
+       #'(lambda (e)
+           (format t "~{ ~a ~}~%" e))
+       triangle))))
+
+;; some additional thoughts:
+;; - don't use any zeroes!
+;; - just have the actual visible numbers
+;; - we can get the (car) and (last (car)) of each "line" and figure out whether to use zero
+;; - maybe do something like: 1st and last are always 1
+;; therefore, just grab everything (butfirst) and (butlast)  (intersection (cdr) (butlast))
+;;
+;;     1
+;;    1  1
+;;   1 2  1
+;; 1  3  3 1
+;;1 4  6  4 1
